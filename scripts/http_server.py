43ae38c14db2fdffed74c54f2d1856f5afca7749
Fernando Maclen <fmaclen@gmail.com>
2023-08-13 12:52:00 -0400

    proof-of-concept web server to run on device

---
+++ scripts/http_server.py
@@ -0,0 +1,63 @@
+import socket
+
+def parse_request(request):
+    headers = {}
+    lines = request.split('\r\n')
+    for line in lines[1:]:
+        if ": " in line:
+            key, value = line.split(": ", 1)
+            headers[key] = value
+    return headers
+
+def main():
+    addr = socket.getaddrinfo('0.0.0.0', 9999)[0][-1]
+
+    s = socket.socket()
+    s.bind(addr)
+    s.listen(1)
+
+    print('listening on', addr)
+
+    while True:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024).decode('utf-8')
+        
+        headers = parse_request(request)
+        
+        if 'OPTIONS' in request:
+            # Handling preflight request
+            response_headers = [
+                "HTTP/1.1 204 No Content",
+                "Access-Control-Allow-Origin: *",
+                "Access-Control-Allow-Methods: POST, OPTIONS",
+                "Access-Control-Allow-Headers: Authorization, Content-Type, Pot-Id",
+                "Access-Control-Max-Age: 3600"  # Cache preflight response for 1 hour
+            ]
+            response = "\r\n".join(response_headers) + "\r\n\r\n"
+            cl.send(response.encode('utf-8'))
+            
+        elif 'POST' in request:
+            if 'Authorization' in headers and 'Pot-Id' in headers:
+                authorization = headers['Authorization']
+                pot_id = headers['Pot-Id']
+
+                # TODO: Validate authorization against PocketBase
+
+                response_headers = [
+                    f"HTTP/1.1 200 OK",
+                    "Content-Type: application/json",
+                    "Access-Control-Allow-Origin: *"  # Required for CORS
+                ]
+                response = "\r\n".join(response_headers)
+                
+                cl.send(response.encode('utf-8'))
+            else:
+                cl.send('HTTP/1.1 400 Bad Request\r\n\r\nMissing required headers.'.encode('utf-8'))
+        else:
+            cl.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode('utf-8'))
+        
+        cl.close()
+
+if __name__ == "__main__":
+    main()
