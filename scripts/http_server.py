91b10bbd581e679b5b372a973c490d29b0ca3c80
Fernando Maclen <fmaclen@gmail.com>
2023-08-14 11:41:07 -0400

    clean up poc again

--- scripts/http_server.py
+++ scripts/http_server.py
@@ -1,4 +1,5 @@
 import socket
+from requests import get
 
 def parse_request(request):
     headers = {}
@@ -38,12 +39,7 @@ def main():
             cl.send(response.encode('utf-8'))
             
         elif 'POST' in request:
-            if 'Authorization' in headers and 'Pot-Id' in headers:
-                authorization = headers['Authorization']
-                pot_id = headers['Pot-Id']
-
-                # TODO: Validate authorization against PocketBase
-
+            if 'Authorization' in headers and 'Pot-Id' in headers and is_valid_jwt(headers['Authorization']):
                 response_headers = [
                     f"HTTP/1.1 200 OK",
                     "Content-Type: application/json",
@@ -53,11 +49,24 @@ def main():
                 
                 cl.send(response.encode('utf-8'))
             else:
-                cl.send('HTTP/1.1 400 Bad Request\r\n\r\nMissing required headers.'.encode('utf-8'))
+                cl.send('HTTP/1.1 400 Bad Request\r\n\r\n'.encode('utf-8'))
         else:
             cl.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode('utf-8'))
         
         cl.close()
+
+
+def is_valid_jwt(jwt: str) -> bool:
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": jwt # e.g. `Bearer aaaa.bbbb.cccc`
+    }
+
+    url = "http://127.0.0.1:8090/api/settings"
+    response = get(url, headers=headers)
+
+    return response.status_code == 200
+
 
 if __name__ == "__main__":
     main()
